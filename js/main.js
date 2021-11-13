d3b923d3257695d042e509ac473cee4e2fee6c14
Alex <algrey18@gmail.com>
2021-11-14 09:38:25 +1000

    scale

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ import './generate-user.js';
 import './draw-avatar.js';
 import './draw-fullscreen.js';
 import './form.js';
+import './scale.js';
 import './slider.js';
--- js/slider.js
+++ js/slider.js
@@ -1,188 +1,86 @@
-// 1. Напишите код, который позволит пользователю редактировать масштаб изображения.
-// Кроме визуального применения эффекта необходимо записывать значение в скрытое поле
-// для дальнейшей отправки на сервер.
 
-// Масштаб:
-// 3) При изменении значения поля .scale__control--value изображению внутри .img-upload__preview
-// должен добавляться соответствующий стиль CSS, который с помощью трансформации scale задаёт масштаб.
-// Например, если в поле стоит значение 75%, то в стиле изображения должно быть написано
-// transform: scale(0.75).
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const imgUploadPreview = document.querySelector('.img-upload__preview');
+const scaleImg = imgUploadPreview.querySelector('img');
 
-const scaleDown = document.querySelector('.scale__control--smaller');
-const scaleUp = document.querySelector('.scale__control--bigger');
-const scaleValue = document.querySelector('.scale__control--value');
-const imgPreview = document.querySelector('.img-upload__preview');
-const noneEffect = document.querySelector('.effecs__radio--none');
-const chromeEffect = document.querySelector('.effects__radio--chrome');
-const sepiaEffect = document.querySelector('.effects__radio--sepia');
-const marvinEffect = document.querySelector('.effects__radio--marvin');
-const phobosEffect = document.querySelector('.effects__radio--phobos');
-const heatEffect = document.querySelector('.effects__radio--heat');
-const effectPreviewChrome = document.querySelector('.effects__preview--chrome');
-
-
-scaleDown.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  const toNumber = parseInt(scaleValue.value, 10);
-  if (toNumber >= 50) {
-    scaleValue.value = `${toNumber - 25  }%`;
-    imgPreview.style.transform = `scale(${parseInt(scaleValue.value, 10) / 100})`;
-  }
-});
-
-scaleUp.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  const toNumber = parseInt(scaleValue.value, 10);
-  if (toNumber <= 75) {
-    scaleValue.value = `${toNumber + 25  }%`;
-    imgPreview.style.transform = `scale(${parseInt(scaleValue.value, 10) / 100})`;
-  }
-});
-
-// 2. С помощью полученных обновлений (стили и скрипты, необходимые для noUiSlider)
-// от Кексобота реализуйте применение эффекта для изображения.
-// Кроме визуального применения эффекта необходимо записывать значение в скрытое поле
-// для дальнейшей отправки на сервер.
-
-//slider
+const effectLevelValue = document.querySelector('.effect-level__value');
+const effectsList = document.querySelector('.effects__list');
+const effectLevel = document.querySelector('.effect-level');
 const slider = document.querySelector('.effect-level__slider');
-const sliderValue = document.querySelector('.effect-level__value');
+
+//добавления фильтра на фото
 
 noUiSlider.create(slider, {
   range: {
     min: 0,
-    max: 100,
+    max: 1,
   },
-  start: 80,
-  step: 1,
+  start: 1,
+  step: 0.1,
   connect: 'lower',
-});
-// let valueEffect = 80;
-
-// подписываюсь на событие изменения слайдера - при перемещении слайдера
-slider.noUiSlider.on('update', (_, handle, unencoded) => {
-  //присваиваю значение переменной, которая ровняется положению слайдера
-  // sliderValue.value = unencoded[handle];
-  sliderValue.setAttribute('value', unencoded[handle]);
-  // перевоже в число
-  // valueEffect = Number(sliderValue.value);
-  //вывожу в консоль
-  console.log(sliderValue.value);
+  format: {
+    to: (value) => Number.isInteger(value) ? value : value.toFixed(1),
+    from: (value) => parseFloat(value),
+  },
 });
 
-// console.log(valueEffect);
+const resetEffects = () => {
+  effectLevel.classList.add('hidden');
+  slider.style.display = 'none';
+  imgUploadPreview.style.filter = '';
+  scaleImg.classList.add('.effects__preview');
+  scaleImg.style.filter = '';
+  scaleImg.classList = '';
+};
 
+resetEffects();
 
-noneEffect.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  imgPreview.classList.remove('effects__preview--chrome');
-  imgPreview.classList.remove('effects__preview--sepia');
-  imgPreview.classList.remove('effects__preview--marvin');
-  imgPreview.classList.remove('effects__preview--phobos');
-  imgPreview.classList.remove('effects__preview--heat');
-});
-
-
-chromeEffect.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  chromeEffect.checked;
-  imgPreview.classList.add('effects__preview--chrome');
-  imgPreview.classList.remove('effects__preview--sepia');
-  imgPreview.classList.remove('effects__preview--marvin');
-  imgPreview.classList.remove('effects__preview--phobos');
-  imgPreview.classList.remove('effects__preview--heat');
-
+const applyEffect = (start, max, min, step, set, filter, measure = '') => {
+  slider.style.display = 'block';
+  scaleImg.classList = '';
+  imgUploadPreview.style = '';
+  effectLevelValue.value = '';
+  effectLevel.classList.remove('hidden');
+  slider.noUiSlider.reset();
+  slider.noUiSlider.set(set);
   slider.noUiSlider.updateOptions({
     range: {
-      min: 0,
-      max: 1,
+      min: min,
+      max: max,
     },
-    start: 0.8,
-    step: 0.1,
+    start: start,
+    step: step,
+  }, true);
+  slider.noUiSlider.on('update', (values, handle) => {
+    effectLevelValue.value = values[handle];
+    imgUploadPreview.style.filter = `${filter}(${values[handle]}${measure})`;
   });
-  // const begin = 'grayscale(';
-  // const end = ')';
-  imgPreview.style.filter = `grayscale(${Number(sliderValue.value)})`;
-  // effectPreviewChrome.style.filter = `grayscale${  valueEffect  }%`;
-});
-// imgPreview.style.filter = valueEffect.value;
+};
 
-
-sepiaEffect.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  sepiaEffect.checked;
-  imgPreview.classList.add('effects__preview--sepia');
-  imgPreview.classList.remove('effects__preview--chrome');
-  imgPreview.classList.remove('effects__preview--marvin');
-  imgPreview.classList.remove('effects__preview--phobos');
-  imgPreview.classList.remove('effects__preview--heat');
-
-  slider.noUiSlider.updateOptions({
-    range: {
-      min: 0,
-      max: 1,
-    },
-    start: 0.8,
-    step: 0.1,
-  });
-
-});
-
-marvinEffect.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  marvinEffect.checked;
-  imgPreview.classList.add('effects__preview--marvin');
-  imgPreview.classList.remove('effects__preview--chrome');
-  imgPreview.classList.remove('effects__preview--sepia');
-  imgPreview.classList.remove('effects__preview--phobos');
-  imgPreview.classList.remove('effects__preview--heat');
-
-  slider.noUiSlider.updateOptions({
-    range: {
-      min: 0,
-      max: 100,
-    },
-    start: 80,
-    step: 1,
-  });
-
+effectsList.addEventListener('change', (evt) => {
+  if (evt.target.matches('#effect-none')) {
+    resetEffects();
+  } else if (evt.target.matches('#effect-chrome')) {
+    applyEffect(1, 1, 0, 0.1, 0, 'grayscale');
+    scaleImg.classList.add('.effects__preview--chrome');
+  } else if (evt.target.matches('#effect-sepia')) {
+    applyEffect(1, 1, 0, 0.1, 0, 'sepia');
+    scaleImg.classList.add('.effects__preview--sepia');
+  } else if (evt.target.matches('#effect-marvin')) {
+    applyEffect(100, 100, 0, 1, 0, 'invert', '%');
+    scaleImg.classList.add('.effects__preview--marvin');
+  } else if (evt.target.matches('#effect-phobos')) {
+    applyEffect(3, 3, 0, 0.1, 0, 'blur', 'px');
+    scaleImg.classList.add('.effects__preview--phobos');
+  } else if (evt.target.matches('#effect-heat')) {
+    applyEffect(3, 3, 1, 0.1, 1, 'brightness');
+    scaleImg.classList.add('.effects__preview--sepia');
+  }
 });
 
-phobosEffect.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  phobosEffect.checked;
-  imgPreview.classList.add('effects__preview--phobos');
-  imgPreview.classList.remove('effects__preview--chrome');
-  imgPreview.classList.remove('effects__preview--sepia');
-  imgPreview.classList.remove('effects__preview--marvin');
-  imgPreview.classList.remove('effects__preview--heat');
-
-  slider.noUiSlider.updateOptions({
-    range: {
-      min: 0,
-      max: 3,
-    },
-    start: 2,
-    step: 0.1,
-  });
-});
-
-heatEffect.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  heatEffect.checked;
-  imgPreview.classList.add('effects__preview--heat');
-  imgPreview.classList.remove('effects__preview--chrome');
-  imgPreview.classList.remove('effects__preview--sepia');
-  imgPreview.classList.remove('effects__preview--marvin');
-  imgPreview.classList.remove('effects__preview--phobos');
-
-  slider.noUiSlider.updateOptions({
-    range: {
-      min: 1,
-      max: 3,
-    },
-    start: 80,
-    step: 0.1,
-  });
-});
-
+export {
+  resetEffects,
+  scaleControlSmaller,
+  scaleControlBigger
+};
